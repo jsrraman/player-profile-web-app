ba1994f0165719604762bcde062b37994dc1a317
Rajaraman <rajaraman.ks@gmail.com>
2015-11-19 20:37:24 +0530

    Add app bar and left navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,36 @@
 "use strict";
 
 var React = require('react');
+var AppBar = require('material-ui/lib/app-bar');
+var FlatButton = require('material-ui/lib/flat-button');
+var LeftNav = require('material-ui/lib/left-nav');
 
 var App = React.createClass({
+    getInitialState: function () {
+        return {
+            menuItems: [
+                {route: 'get-started', text: 'Get Started'},
+                {route: 'customization', text: 'Customization'},
+                {route: 'components', text: 'Components'}
+            ]
+        };
+    },
+
+    _toggleNav: function () {
+        this.refs.leftNav.toggle();
+    },
+
     render: function () {
         return (
             <div>
-                <h1>Hello World</h1>
+                <AppBar
+                    title="Player Profile"
+                    onLeftIconButtonTouchTap={this._toggleNav}/>
+
+                <LeftNav
+                    ref="leftNav"
+                    docked={false}
+                    menuItems={this.state.menuItems}/>
             </div>
         )
     }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 "use strict";
 
 var React = require('react');
+var ReactDOM = require('react-dom');
 var App = require('./components/App');
 
-React.render(<App/>, document.getElementById("App"));
+// Note:
+// In order the tap events to work we need the following work around
+//http://material-ui.com/#/get-started/installation
+var injectTapEventPlugin = require("react-tap-event-plugin");
+
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
+
+ReactDOM.render(<App/>, document.getElementById("App"));
