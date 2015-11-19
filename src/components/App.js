c34c2923bec07fee55efca95bc2a695f29a51d34
Rajaraman <rajaraman.ks@gmail.com>
2015-11-19 17:05:06 +0530

    Add necessary files for gulp configuration to get started

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var React = require('react');
+
+var App = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        )
+    }
+});
+
+module.exports = App;
